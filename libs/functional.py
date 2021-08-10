aa5daaf00bbb3e4792bf008abd0967ec3c5f8f2b
fan84sunny <88661130+fan84sunny@users.noreply.github.com>
2021-08-10 15:58:24 +0800

    Update functional.py
    Q1第一大題_范馨云

--- libs/functional.py
+++ libs/functional.py
@@ -16,4 +16,16 @@ class Polynomial:
         self.a = np.array(a)
 
     def __call__(self, x: Union[float, int]) -> Variable:
-        pass
+        f_value=0
+        f_grad=0
+        # 計算function
+        for i in range(self.a.size):
+          f_value += self.a[i] * x**i
+          
+        # 計算微分
+        for i in range(self.a.size-1):
+          f_grad += self.a[i+1] *(i+1) * x**i
+
+        # 計算結果儲存至Variable  
+        function=Variable(f_value,f_grad)
+        return function
