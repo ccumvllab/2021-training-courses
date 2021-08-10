7fc8c4744cdef1035bc5ce0bf41f4cf68caa5899
fan84sunny <88661130+fan84sunny@users.noreply.github.com>
2021-08-10 16:01:40 +0800

    Update visualization.py
    范馨云作業繳交

--- libs/visualization.py
+++ libs/visualization.py
@@ -1,5 +1,14 @@
 import matplotlib
+import matplotlib.pyplot as plt
 
-
-def plot_polynomial():
-    pass
+def plot_polynomial(x_cut, a: List = None):
+    f = Polynomial(a)
+    x=np.linspace(x_cut-10 ,x_cut+10 ,1024) # x軸 (x_start, x_stop, y_tall)
+    y=f(x).value  # y軸
+    
+    plt.plot(x,y,color='blue',label='polynomial line')  # function graph
+    plt.plot(x, (x-x_cut) *f(x_cut).grad +f(x_cut).value ,color='red',label='tangent line') # tangent line
+    
+    plt.legend(loc='best',title='Polynomial graph_fanxinyun') # 標籤放置
+    plt.savefig('plot_polynomial.png') #在show之前要先save
+    plt.show()   #show圖
