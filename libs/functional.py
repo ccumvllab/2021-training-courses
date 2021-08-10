bcb58d4901065c86fc5f8a92dca9f76c1b1a7231
Hao-Ting Li <maniac@csie.io>
2021-08-10 16:28:12 +0800

    feat: add array case

--- libs/functional.py
+++ libs/functional.py
@@ -15,11 +15,21 @@ class Polynomial:
     def __init__(self, a: List = None):
         self.a = np.array(a)
 
-    def __call__(self, x: Union[float, int]) -> Variable:
-        f = np.power(x, range(len(self.a)))
-        var = Variable(value=sum(self.a * f))
+    def __call__(self, x: Union[float, np.ndarray]) -> Variable:
+        if isinstance(x, np.ndarray):
+            _x = x.repeat(len(self.a)).reshape(len(x), len(self.a))
+            f = np.power(_x, range(len(self.a)))
+            var = Variable(value=np.sum(self.a * f, 1))
 
-        coef = np.arange(1, len(self.a))
-        grad_f = np.power(x, range(len(self.a) - 1))
-        var.grad = sum(coef * self.a[1:] * grad_f)
-        return var
+            coef = np.arange(1, len(self.a))
+            grad_f = np.power(_x[:, :-1], range(len(self.a) - 1))
+            var.grad = np.sum(coef * self.a[1:] * grad_f, 1)
+            return var
+        else:
+            f = np.power(x, range(len(self.a)))
+            var = Variable(value=sum(self.a * f))
+
+            coef = np.arange(1, len(self.a))
+            grad_f = np.power(x, range(len(self.a) - 1))
+            var.grad = sum(coef * self.a[1:] * grad_f)
+            return var
