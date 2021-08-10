71c367eeab51e8c029310c265e7ff6fd670de7ba
Hao-Ting Li <maniac@csie.io>
2021-08-10 23:42:02 +0800

    feat: add `plot_polynomial()`

--- libs/visualization.py
+++ libs/visualization.py
@@ -1,5 +1,16 @@
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def plot_polynomial():
-    pass
+def plot_polynomial(file, function, x):
+    X = np.linspace(0, 100)
+    Y_f = function(X)
+
+    y = function(x)
+    slope = y.grad
+    Y_tangent = slope * (X - x) + y.value
+
+    fig, ax = plt.subplots()
+    ax.plot(X, Y_f.value)
+    ax.plot(X, Y_tangent)
+    fig.savefig(file)
