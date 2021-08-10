5cbc950a0ac5412e248b55c0a662d00190472afe
Hao-Ting Li <maniac@csie.io>
2021-08-10 21:46:07 +0800

    fix: remove input `int` type refactor: remove duplicated code

--- libs/functional.py
+++ libs/functional.py
@@ -16,20 +16,15 @@ class Polynomial:
         self.a = np.array(a)
 
     def __call__(self, x: Union[float, np.ndarray]) -> Variable:
-        if isinstance(x, np.ndarray):
-            _x = x.repeat(len(self.a)).reshape(len(x), len(self.a))
-            f = np.power(_x, range(len(self.a)))
-            var = Variable(value=np.sum(self.a * f, 1))
+        if isinstance(x, float):
+            x = np.array([x])
 
-            coef = np.arange(1, len(self.a))
-            grad_f = np.power(_x[:, :-1], range(len(self.a) - 1))
-            var.grad = np.sum(coef * self.a[1:] * grad_f, 1)
-            return var
-        else:
-            f = np.power(x, range(len(self.a)))
-            var = Variable(value=sum(self.a * f))
+        x = x.repeat(len(self.a)).reshape(len(x), len(self.a))
 
-            coef = np.arange(1, len(self.a))
-            grad_f = np.power(x, range(len(self.a) - 1))
-            var.grad = sum(coef * self.a[1:] * grad_f)
-            return var
+        f = np.power(x, range(len(self.a)))
+        coef = np.arange(1, len(self.a))
+        grad_f = np.power(x[:, :-1], range(len(self.a) - 1))
+
+        var = Variable(value=np.sum(self.a * f, 1))
+        var.grad = np.sum(coef * self.a[1:] * grad_f, 1)
+        return var
