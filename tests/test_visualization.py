4b90199eb326a780330f5b355c0ef009159d179b
Hao-Ting Li <maniac@csie.io>
2021-08-14 13:04:50 +0800

    feat: add `test_plot_regression()`

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 import numpy as np
 
-from libs.functional import Polynomial
-from libs.visualization import plot_polynomial
+from libs.functional import Polynomial, regression_sgd
+from libs.visualization import plot_polynomial, plot_regression
 
 
 class TestPlotPolynomial:
@@ -21,3 +21,20 @@ class TestPlotPolynomial:
         x = np.linspace(-2.5, 1.0)
         plot_polynomial(file=file, function=f, p_x=-1., x=x, formula='4 + x + 3x^2 + 1.2x^3')
         assert file.exists()
+
+
+def test_plot_regression():
+    m_gt, b_gt = 2.4, 1.0
+    num_samples = 100
+    x = np.random.uniform(-10, 10, num_samples)
+    noise = np.random.normal(0, 1, num_samples)
+    y = m_gt * x + b_gt + noise
+    num_iterations = 1000
+    batch_size = 10
+    learning_rate = 0.001
+
+    m, b = regression_sgd(x, y, num_samples, num_iterations, batch_size, learning_rate)
+    file = Path('test_plot_regression.png')
+    plot_regression(file, x, y, prediction=(m, b), groundtruth=(m_gt, b_gt))
+
+    assert file.exists()
