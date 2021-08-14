d13574bf30af3acde2428748b3156d44fd4a09b8
Hao-Ting Li <maniac@csie.io>
2021-08-14 13:05:31 +0800

    feat: add solutions for Q2

--- libs/functional.py
+++ libs/functional.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Union, Tuple
 
 import numpy as np
 
@@ -28,3 +28,29 @@ class Polynomial:
         var = Variable(value=np.sum(self.a * f, 1))
         var.grad = np.sum(coef * self.a[1:] * grad_f, 1)
         return var
+
+
+def regression_sgd(x, y, num_samples, num_iterations, batch_size, learning_rate) -> Tuple[np.ndarray, np.ndarray]:
+    idx = np.arange(num_samples)
+    np.random.shuffle(idx)
+
+    m = np.zeros(num_iterations + 1)
+    b = np.zeros(num_iterations + 1)
+
+    m[0] = np.random.normal()
+    b[0] = np.random.normal()
+
+    for i in range(num_iterations):
+        j = slice(i * batch_size % num_samples, (i + 1) * batch_size % num_samples)
+        x_in_batch = x[j]
+        y_in_batch = y[j]
+
+        # Compute the gradients
+        m_grad = np.sum(2 * x_in_batch * (m[i] * x_in_batch + b[i] - y_in_batch)) / num_samples
+        b_grad = np.sum(2 * (m[i] * x_in_batch + b[i] - y_in_batch)) / num_samples
+
+        # Update m and b
+        m[i + 1] = m[i] - learning_rate * m_grad
+        b[i + 1] = b[i] - learning_rate * b_grad
+
+    return m, b
--- libs/visualization.py
+++ libs/visualization.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 plt.style.use('seaborn-bright')
 plt.rcParams.update({
@@ -26,4 +27,20 @@ def plot_polynomial(file, function, p_x, x=None, formula=None):
     ax.plot(x, y_tangent)
     ax.plot(p_x, p_y.value, 'o', markersize=3)
     ax.legend([rf'$f(x) = {formula}$', rf'the tangent at the point $p = {p_x}$', r'the point $p$'])
+    fig.savefig(file)
+
+
+def plot_regression(file, x, y, prediction, groundtruth):
+    m, b = prediction
+    m_gt, b_gt = groundtruth
+
+    x_line = np.linspace(-10, 10)
+    y_line = m[-1] * x_line + b[-1]
+    y_line_gt = m_gt * x_line + b_gt
+
+    fig, ax = plt.subplots()
+    ax.plot(x_line, y_line, alpha=0.7)
+    ax.plot(x, y, 'o', markersize=3, alpha=0.7)
+    ax.plot(x_line, y_line_gt, alpha=0.7)
+    ax.legend(['Prediction', 'Data points', 'Groundtruth'])
     fig.savefig(file)
