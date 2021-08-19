694799a7efeb869b0d0adf5874231332e40e2bc2
rmxkyz <rmxkyz>
2021-08-20 06:00:38 +0800

    q2

--- libs/functional.py
+++ libs/functional.py
@@ -1,3 +1,4 @@
+#https://towardsdatascience.com/step-by-step-tutorial-on-linear-regression-with-stochastic-gradient-descent-1d35b088a843
 from typing import List, Union, Tuple
 
 import numpy as np
@@ -20,4 +21,20 @@ class Polynomial:
 
 
 def regression_sgd(x, y, num_samples, num_iterations, batch_size, learning_rate) -> Tuple[np.ndarray, np.ndarray]:
-    pass
+    m,b = np.zeros(num_iterations+1), np.zeros(num_iterations+1) #array saving updated m,b
+    m[0],b[0] = np.random.randn(),np.random.randn() #initial random value data points
+    samples  = list(zip(x,y))
+    np.random.shuffle(samples)
+    for i in range(num_iterations):
+        temp_m,temp_b = m[i],b[i]
+        for j in range(0,num_samples,batch_size):
+            x0 = np.array([x for x,_ in samples[j:j+batch_size]]) #samples in x
+            y0 = np.array([y for _,y in samples[j:j+batch_size]]) #samples in y
+            y1 = temp_m * x0 + temp_b  #y-hat = mx+b
+            m_grad = np.sum(2*x0*(y1-y0))/batch_size #(2x(mx+b-y))/batch_size = (2x(y1-y0))/batch_size
+            b_grad = np.sum(2*(y1-y0))/batch_size #(2(mx+b-y))/batch_size = (2(y1-y0))/batch_size
+            temp_m -= learning_rate*m_grad
+            temp_b -= learning_rate*b_grad
+        m[i+1] = temp_m
+        b[i+1] = temp_b
+    return (m,b)
--- libs/visualization.py
+++ libs/visualization.py
@@ -1,6 +1,18 @@
+import matplotlib.pyplot as plt
+
 def plot_polynomial(file, function, p_x, x=None, formula=""):
     pass
 
 
 def plot_regression(file, x, y, prediction, groundtruth):
-    pass
+    plt.figure(figsize=(8,6),dpi=150)
+    gt_poly = groundtruth[0]*x + groundtruth[1]
+    pd_poly = prediction[0][-1]*x + prediction[1][-1]
+    plt.scatter(x, y, s=15, c='lime', label='Data points', alpha=0.7, zorder=2)
+    plt.plot(x, pd_poly, c="blue", linewidth=1, label='Groundtruth', alpha=0.7, zorder=1)
+    plt.plot(x, gt_poly, c="red", linewidth=1, label='Prediction', alpha=0.7, zorder=3)
+    handles, labels = plt.gca().get_legend_handles_labels()
+    order = [1,2,0]
+    plt.legend([handles[idx] for idx in order],[labels[idx] for idx in order])
+    plt.savefig(file)
+    plt.show()
