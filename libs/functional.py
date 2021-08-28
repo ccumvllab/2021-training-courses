c6782454cce168d45edcb6cc4c7e78714abeebee
lyjeff <38328932+lyjeff@users.noreply.github.com>
2021-08-28 23:45:43 +0800

    Update Q1 Finish two part of Q1, save the output images to `tests/`

--- libs/functional.py
+++ libs/functional.py
@@ -5,18 +5,42 @@ import numpy as np
 
 class Variable:
 
-    def __init__(self, value=None):
+    def __init__(self, value=None, grad=None):
         self.value = value
-        self.grad = None
+        self.grad = grad
 
 
 class Polynomial:
 
     def __init__(self, a: List = None):
-        self.a = np.array(a)
+        self.coef = np.array(a)
 
     def __call__(self, x: Union[float, int]) -> Variable:
-        pass
+        x = np.array(x)
+        base = np.repeat(x, len(self.coef)).reshape(x.size, len(self.coef))
+
+        exponent = np.arange(0, len(self.coef))
+
+        # get the power from base and exponent, power=base^exponent
+        power = np.power(base, exponent)
+
+        # get the result of the polynomial
+        y = np.sum(np.multiply(self.coef, power), axis=1)
+
+        # get the coefficient of the differential polynomial
+        coef_diff = np.multiply(self.coef[1:], np.arange(1, len(self.coef)))
+
+        base_diff = np.repeat(x, len(coef_diff)).reshape(
+            x.size, len(coef_diff))
+        exponent_diff = np.arange(0, len(coef_diff))
+
+        # get power
+        power_diff = np.power(base_diff, exponent_diff)
+
+        # get the result of the polynomial
+        y_diff = np.sum(np.multiply(coef_diff, power_diff), axis=1)
+
+        return Variable(y, y_diff)
 
 
 def regression_sgd(x, y, num_samples, num_iterations, batch_size, learning_rate) -> Tuple[np.ndarray, np.ndarray]:
--- libs/visualization.py
+++ libs/visualization.py
@@ -1,5 +1,30 @@
+import matplotlib.pyplot as plt
+import matplotlib
+
+
 def plot_polynomial(file, function, p_x, x=None, formula=""):
-    pass
+
+    y = function(x).value
+    p_y = function(p_x).value
+
+    p_grad = function(p_x).grad
+    tangent_y = x * p_grad + p_y - p_grad * p_x
+
+    matplotlib.rcParams['mathtext.fontset'] = 'cm'
+    matplotlib.rcParams['font.family'] = 'Times New Roman'
+    matplotlib.rcParams['font.size'] = 12
+
+    plt.xlabel("x-axis")
+    plt.ylabel("y-axis")
+
+    plt.plot(
+        x, y, label=f"$f(x)={formula}$")
+    plt.plot(x, tangent_y, label=f"the tangent at the point $p={p_x}$")
+    plt.plot(p_x, p_y, 'o', label=f"the point $p$")
+
+    plt.legend(loc="best")
+    plt.savefig(file, dpi=150)
+    plt.show()
 
 
 def plot_regression(file, x, y, prediction, groundtruth):
